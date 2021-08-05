1df162b8618d461511f8646364feb95d2d909370
Jacob Stordahl <jacob@stordahl.dev>
2021-08-04 20:42:31 -0500

    Tweaking pipeline

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,21 @@
 "use strict";
 const gulp = require("gulp");
-const sass = require('gulp-sass')(require('node-sass'));
+const sass = require('gulp-dart-sass');
 const concat = require("gulp-concat");
 const sassdoc = require('sassdoc');
 
-gulp.task("sass", () => {
-
-  const options = {
-    dest: 'docs',
-    verbose: true
-  };
+gulp.task('compile', () => {
+  const options = { dest: 'docs', verbose: true };
 
   return gulp
-    .src("./src/*.scss")
-    .pipe(concat("ignite.scss"))
+    .src('./src/*.scss')
     .pipe(sassdoc(options))
-    .pipe(sass().on("error", sass.logError))
-    .pipe(gulp.dest("./dist/"));
+    .pipe(sass({outFile: './dist/ignite.css'}).on('error', sass.logError))
+    .pipe(gulp.dest('./dist/'));
+});
+
+//broken??
+gulp.task('watch', () => {
+  //@ts-ignore
+  return gulp.watch('./sass/*.scss', ['compile']);
 });
