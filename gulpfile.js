41ade5dc3389890110f9949825f9aaf9e9ca71a8
Jacob Stordahl <jacob@stordahl.dev>
2021-08-05 17:38:23 -0500

    Finalizing workflow

--- gulpfile.js
+++ gulpfile.js
@@ -3,16 +3,35 @@ const gulp = require("gulp");
 const sass = require('gulp-dart-sass');
 const concat = require("gulp-concat");
 const sassdoc = require('sassdoc');
+const postcss = require('gulp-postcss');
+
 
 gulp.task('compile', () => {
-  const options = { dest: 'docs', verbose: true };
+  const options = { dest: 'src-docs', verbose: true };
 
   return gulp
     .src('./src/*.scss')
     .pipe(sassdoc(options))
-    .pipe(sass({outFile: './dist/ignite.css'}).on('error', sass.logError))
+    .pipe(sass({outFile: './dist/style.css'}).on('error', sass.logError))
     .pipe(gulp.dest('./dist/'));
+}); 
+
+
+gulp.task('generate', () => {
+	return gulp
+    .src('./dist/style.css')
+    .pipe(
+		  postcss([
+			  require('mdcss')({
+          destination: './dist/docs',
+          assets: ['./dist/style.css'],
+        })
+		  ]))
+    .pipe(
+		  gulp.dest('./dist/')
+	  );
 });
+
 
 //broken??
 gulp.task('watch', () => {
